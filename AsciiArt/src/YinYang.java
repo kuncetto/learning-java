a2e46fa83cc74a50f49700f0734f7c45b627c94b
Concetto Condorelli <concettocondorelli@gmail.com>
2018-03-03 19:48:16 +0100

    Add AsciiArt Project

---
+++ AsciiArt/src/YinYang.java
@@ -0,0 +1,10 @@
+public class YinYang {
+    public static void main(String[] args) {
+        // ASCII art - https://it.wikipedia.org/wiki/ASCII_art
+        System.out.println("    __     ");
+        System.out.println("   /o)\\   ");
+        System.out.println("   \\(o/   ");
+        System.out.println();
+        System.out.println("Yin & Yang");
+    }
+}
